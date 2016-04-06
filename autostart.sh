777648e170dead4c4b1d2cfdabd8611686305460
Iver Egge <iverasp@gmail.com>
2016-04-06 21:02:05 +0200

    done

--- autostart.sh
+++ autostart.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-usage="$(basename "$0") [-a] [-h] vm-name -- program to set autostart of vm(s)
+usage="$(basename "$0") [-n] [-a] [-h] -- program to set autostart of vm(s)
 
 where:
     -h  show this help text
-    -a  do for all vms (in xe vm-list)"
+    -a  do for all vms (in xe vm-list)
+    -n do for vm with name"
 
-while getopts ':hs:' option; do
+function all {
+    VMS="$(xe vm-list | grep uuid | cut -d":" -f2 | cut -d" " -f2)"
+    for vm in $VMS; do
+        xe vm-param-set uuid="$vm" other-config:auto_poweron=true
+    done
+}
+
+function add-vm {
+    VM="$(xe vm-list | grep -B 1 -w $1 | grep uuid | cut -d":" -f2 | cut -d" " -f2)"
+    echo $VM
+    xe vm-param-set uuid="$VM" other-config:auto_poweron=true
+}
+
+while getopts ':han' option; do
   case "$option" in
     h) echo "$usage"
        exit
        ;;
-    s) seed=$OPTARG
+    a) all
        ;;
-    :) printf "missing argument for -%s\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
+    n) for last; do true; done
+       add-vm $last
        ;;
    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -25,14 +38,3 @@ while getopts ':hs:' option; do
 done
 shift $((OPTIND - 1))
 
-function add-all {
-    VMS="$(xe vm-list | grep uuid | cut -d":" -f2)"
-    for vm in VMS; do
-        echo $vm
-    done
-}
-
-function add-vm {
-    VM="$(xe vm-list | grep -B 1 -w $1 | grep uuid | cut -d":" -f2)"
-    echo $VM
-}
